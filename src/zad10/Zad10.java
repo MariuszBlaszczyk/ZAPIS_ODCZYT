4557e41e230cb392371fc50dbcacd734ea131842
Mariusz Błaszczyk <mariusz.blaszczyk@tuta.io>
2021-07-19 12:29:31 +0200

    ups

--- src/zad10/Zad10.java
+++ src/zad10/Zad10.java
@@ -6,5 +6,22 @@ w postaci liczba1;liczba2;liczba3;… .
 Następnie pobierz zawartość pliku ‘liczby.txt’ i oblicz odchylenie standardowe pobranych liczb.
  */
 
+import java.util.Random;
+
 public class Zad10 {
+
+    static int generateNumbers(int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException("Min is bigger than max!");
+        }
+        return new Random().nextInt(max - min + 1) + min;
+    }
+
+    static void saveToFile
+
+    public static void main(String[] args) {
+
+        int min = 20;
+        int max = 45;
+    }
 }
